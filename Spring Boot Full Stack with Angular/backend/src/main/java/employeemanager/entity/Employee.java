215a9c66c308d7816e8353544c33fa45dde6ea27
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-08-11 16:31:17 +0300

    add project properties

--- Spring Boot Full Stack with Angular/backend/src/main/java/employeemanager/entity/Employee.java
+++ Spring Boot Full Stack with Angular/backend/src/main/java/employeemanager/entity/Employee.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "employees")
 public class Employee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

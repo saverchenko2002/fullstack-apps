6022cefbac2653cc62f8db2f39ba8d96d652d9ef
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-08-11 16:43:35 +0300

    add repo service and exception

---
+++ Spring Boot Full Stack with Angular/backend/src/main/java/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package employeemanager.service;
+
+import employeemanager.entity.Employee;
+import employeemanager.exception.UserNotFoundException;
+import employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by ID=" + id + " was not found."));
+    }
+}

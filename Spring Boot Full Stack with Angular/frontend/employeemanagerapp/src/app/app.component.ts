12860c793af62c2ca497bcd5bfe2d9c7cd2bea38
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-08-11 21:52:01 +0300

    add html snippet

--- Spring Boot Full Stack with Angular/frontend/employeemanagerapp/src/app/app.component.ts
+++ Spring Boot Full Stack with Angular/frontend/employeemanagerapp/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit {
+  public employees: Employee[];
+  
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+  
+  public getEmployees() :void {
+    this.employeeService.getEmployees().subscribe(data=>{
+      this.employees=data;
+    }, error => console.log(error));
+  }
+
 }
